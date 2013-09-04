50bde1a4e8e7491e813bb5b9261c343db867281b
Jason Michalski <armooo@armooo.net>
2013-09-04 08:54:29 -0400

    Add description

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 setup(
     name='suds_requests',
     version='0.1',
+    description=open('README.rst').read(),
     author='Jason Michalski',
     author_email='armooo@armooo.net',
     py_modules=['suds_requests'],
