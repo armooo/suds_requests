baad1df42e5615d99b69d64d83d13de54ccca572
Jason Michalski <armooo@armooo.net>
2015-07-07 22:30:48 -0700

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='suds_requests',
-    version='0.1',
+    version='0.2',
     description=open('README.rst').read(),
     author='Jason Michalski',
     author_email='armooo@armooo.net',
