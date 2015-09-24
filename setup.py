2e87f936caa1c435a41dc60b06d7295be88597b5
Jason Michalski <armooo@armooo.net>
2015-09-23 19:18:12 -0700

    Bump version to 0.3
    Fixes #3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='suds_requests',
-    version='0.2',
+    version='0.3',
     description='A suds transport implemented with requests',
     long_description=open('README.rst').read(),
     author='Jason Michalski',
