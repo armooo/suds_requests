a86f79502afc4cfc91d36166d445ade1ae4ef2e4
Jason Michalski <armooo@armooo.net>
2017-01-17 21:13:11 -0800

    Create the 0.3.1 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='suds_requests',
-    version='0.3',
+    version='0.3.1',
     description='A suds transport implemented with requests',
     long_description=open('README.rst').read(),
     author='Jason Michalski',
